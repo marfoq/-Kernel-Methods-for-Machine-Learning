5011cf574d94846f1b4abec92b4a536f9468879c
pvnieo <sohaib.attaiki@gmail.com>
2019-03-20 19:43:08 +0100

    first commit

---
+++ classifiers/logistic_regression.py
@@ -0,0 +1,13 @@
+# 3p
+import numpy as np
+
+
+class LogisticRegression:
+    def __init__(self,):
+        pass
+
+    def fit(self, K_train, y):
+        pass
+
+    def predict(self, K_test):
+        pass
